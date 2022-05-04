820f26f714e7db4541ee5408673b86b109a8cd67
yaroslav-svirida <yaroslav.svirida@gmail.com>
2022-05-04 21:14:47 +0300

    Дз+04,05 занятие

--- video_hosting/comment_view.py
+++ video_hosting/comment_view.py
@@ -1,5 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from video_hosting.models import *
@@ -26,3 +28,17 @@ class CommentView(APIView):
 
         comment = Comment.objects.create(video=video, content=content)
         comment_serialized= CommentSerializers(comment).data
+        return Response(comment_serialized)
+
+    def get(self, pk=None):
+        try:
+            if not pk:
+                comments = Comment.objects.all()
+                comment_serialized = CommentSerializers(comments, many=True).data
+                return Response(comment_serialized)
+            comment = Comment.objects.get(id=pk)
+            comment_serialized = CommentSerializers(comment).data
+            return Response(comment_serialized)
+        except ObjectDoesNotExist as e:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
--- video_hosting/urls.py
+++ video_hosting/urls.py
@@ -7,8 +7,14 @@ urlpatterns = [
     path('v1/video/', VideoAPIList.as_view()),
     path('v1/video/<int:pk>', VideoAPIListSingle.as_view()),
 
-    path('comment/create/', CommentView.as_view()),
+
 
     path('v1/comment/', CommentAPIList.as_view()),
     path('v1/comment/<int:pk>', CommentAPIListSingle.as_view()),
+]
+
+urlpatterns +=[
+    path('comment/create/', CommentView.as_view()),
+    path('comment/get/<int:pk>', CommentView.as_view()),
+    path('comment/get/', CommentView.as_view())
 ]
